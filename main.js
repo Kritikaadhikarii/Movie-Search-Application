190a692ae22010efff1f98c6c523aba8b2d8a459
Kritikaadhikarii <itskritikaaa@gmail.com>
2023-07-06 09:32:51 +0545

    new changes made

--- main.js
+++ main.js
@@ -1,59 +1,37 @@
     // https://www.omdbapi.com/?s=game&apikey=820658f7
     // http://www.omdbapi.com/?i=tt3896198&apikey=820658f7
-    async function fetchData(){
-        const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=820658f7');
-        const datas = await response.json();
-        console.log(datas)
-        var titlevar = document.getElementById("Title")
-        titlevar.textContent = datas.Title;
 
-        var typevar = document.getElementById("Type")
-        typevar.textContent = datas.Type;
-
-        var yearvar = document.getElementById("Year")
-        yearvar.textContent = datas.Year;
-
-        var plotvar = document.getElementById("Plot")
-        plotvar.textContent = datas.Plot;
+    const movieContainer = document.getElementById("movie-container");
 
 
-        var image = document.getElementById("Poster")
-        image.src = datas.Poster;
+    const search = document.getElementById("search");
 
-        // for search
 
-        var searchbtn= document.getElementById("search")
-        searchbtn.addEventListener('click',async function(){
-        var inputfield = document.getElementById("movie").value 
-        console.log(inputfield)
+async function loadMoreData(){
+    var inputfield = document.getElementById("movie").value 
+    console.log(inputfield)
+    const response = await fetch(`http://www.omdbapi.com/?s=${inputfield}&apikey=820658f7`);
+    const datas = await response.json();
 
-        // inputfield.innerhtml='';
+    for(let i =0; i<datas.Search.length;i++){
+        const movieData = datas.Search[i];
 
-        // // use if else
-        // if (inputfield === '');
+        const card = document.createElement("div");
+        card.className = "card";
 
-        const response = await fetch(`http://www.omdbapi.com/?s=${inputfield}&apikey=820658f7`);
-        const datas = await response.json();
-        console.log(datas.Search[0])
-        var titlevar = document.getElementById("Title")
-        titlevar.textContent = datas.Search[0].Title;
+        const movieTitle = document.createElement("h1");
+        movieTitle.className = "title";
+        movieTitle.innerHTML = movieData.Title
+        card.appendChild(movieTitle)
 
-        var typevar = document.getElementById("Type")
-        typevar.textContent = datas.Search[0].Type;
-
-        var yearvar = document.getElementById("Year")
-        yearvar.textContent = datas.Search[0].Year;
-
-        var plotvar = document.getElementById("Plot")
-        plotvar.textContent = datas.Search[0].Plot;
-
-        var image = document.getElementById("Poster")
-        image.src = datas.Search[0].Poster;
-
-       })
+        movieContainer.appendChild(card)
+    }
 
 }
 
+search.addEventListener("click",()=>{
+    loadMoreData();
+})
 
 
-fetchData();
+//  fetchData()
