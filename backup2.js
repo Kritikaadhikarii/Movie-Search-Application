1e11dc443a192c996df92ef291b57d3cb28595ea
Kritikaadhikarii <itskritikaaa@gmail.com>
2023-07-07 19:12:27 +0545

    new changes made

---
+++ backup2.js
@@ -0,0 +1,111 @@
+    // https://www.omdbapi.com/?s=game&apikey=820658f7
+    // http://www.omdbapi.com/?i=tt3896198&apikey=820658f7
+
+    const movieContainer = document.getElementById("movie-container");
+
+
+    const search = document.getElementById("search");
+
+
+async function loadMoreData(){
+    var inputfield = document.getElementById("movie").value 
+    console.log(inputfield)
+
+    movieContainer.innerHTML = '';
+    //this resolved the issue of search button not working after one search
+
+    const response = await fetch(`http://www.omdbapi.com/?s=${inputfield}&apikey=820658f7`);
+    const datas = await response.json();
+
+
+
+
+// ------------------------------------------------------
+  if (!datas || !datas.Search ) {
+    const errorCard = document.createElement("div");
+    errorCard.className = "ecard";
+
+    const errorMessage = document.createElement("h1");
+    errorMessage.className = "error-message";
+    errorMessage.innerHTML = "404 Error: Movie not found. Try Again!";
+    errorCard.appendChild(errorMessage);
+    alert("404 error")
+
+    movieContainer.appendChild(errorCard);
+    return;
+    
+  }
+// ------------------------------------------------
+
+
+// if (inputfield === '') {
+//     async function fetchData(){
+//         const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=820658f7');
+//         const datas = await response.json();
+//         console.log(datas)
+//         var titlevar = document.getElementById("Title")
+//         titlevar.textContent = datas.Title;
+
+//         var typevar = document.getElementById("Type")
+//         typevar.textContent = datas.Type;
+
+//         var yearvar = document.getElementById("Year")
+//         yearvar.textContent = datas.Year;
+
+//         var plotvar = document.getElementById("Plot")
+//         plotvar.textContent = datas.Plot;
+
+
+//         var image = document.getElementById("Poster")
+//         image.src = datas.Poster;
+//     }
+
+
+//   ---------
+
+// --------------------------------------------------------
+
+
+    for(let i =0; i<datas.Search.length;i++){
+        const movieData = datas.Search[i];
+
+        const card = document.createElement("div");
+        card.className = "card";
+
+        const movieTitle = document.createElement("h1");
+        movieTitle.className = "title";
+        movieTitle.innerHTML = movieData.Title
+        card.appendChild(movieTitle)
+
+        const movieYear = document.createElement("h2");
+        movieYear.className = "Year";
+        movieYear.innerHTML = movieData.Year
+        card.appendChild(movieYear)
+
+        const imdbID = document.createElement("h3");
+        imdbID.className = "imdbID";
+        imdbID.innerHTML = movieData.imdbID
+        card.appendChild(imdbID)
+
+        const movieType = document.createElement("h2");
+        movieType.className = "Type";
+        movieType.innerHTML = movieData.Type
+        card.appendChild(movieType)
+
+        const movieImage = document.createElement("img");
+        movieImage.className = "Poster";
+        movieImage.src = movieData.Poster;
+        movieImage.alt = movieData.Title;
+        card.appendChild(movieImage);
+
+
+        movieContainer.appendChild(card)
+    }
+
+}
+
+search.addEventListener("click",()=>{
+    loadMoreData();
+})
+
+
--- main.js
+++ main.js
@@ -10,28 +10,124 @@
 async function loadMoreData(){
     var inputfield = document.getElementById("movie").value 
     console.log(inputfield)
+
+    movieContainer.innerHTML = '';
+    //this resolved the issue of search button not working after one search
+
     const response = await fetch(`http://www.omdbapi.com/?s=${inputfield}&apikey=820658f7`);
     const datas = await response.json();
 
-    for(let i =0; i<datas.Search.length;i++){
-        const movieData = datas.Search[i];
+    if(inputfield === ""){
+        // fetchData()
+    }else{
+        for(let i =0; i<datas.Search.length;i++){
+            const movieData = datas.Search[i];
+    
+            const card = document.createElement("div");
+            card.className = "card";
+    
+            const movieTitle = document.createElement("h1");
+            movieTitle.className = "title";
+            movieTitle.innerHTML = movieData.Title
+            card.appendChild(movieTitle)
+    
+            const movieYear = document.createElement("h2");
+            movieYear.className = "Year";
+            movieYear.innerHTML = movieData.Year
+            card.appendChild(movieYear)
+    
+            const imdbID = document.createElement("h3");
+            imdbID.className = "imdbID";
+            imdbID.innerHTML = movieData.imdbID
+            card.appendChild(imdbID)
+    
+            const movieType = document.createElement("h2");
+            movieType.className = "Type";
+            movieType.innerHTML = movieData.Type
+            card.appendChild(movieType)
+    
+            const movieImage = document.createElement("img");
+            movieImage.className = "Poster";
+            movieImage.src = movieData.Poster;
+            movieImage.alt = movieData.Title;
+            card.appendChild(movieImage);
+    
+    
+            movieContainer.appendChild(card)
+        }
 
-        const card = document.createElement("div");
-        card.className = "card";
-
-        const movieTitle = document.createElement("h1");
-        movieTitle.className = "title";
-        movieTitle.innerHTML = movieData.Title
-        card.appendChild(movieTitle)
-
-        movieContainer.appendChild(card)
     }
 
+  
+
+    
+// ------------------------------------------------------
+  if (!datas || !datas.Search) {
+    const errorCard = document.createElement("div");
+    errorCard.className = "ecard";
+
+    const errorMessage = document.createElement("h1");
+    errorMessage.className = "error-message";
+    errorMessage.innerHTML = "404 Error: Movie not found. Try Again!";
+    errorCard.appendChild(errorMessage);
+    alert("404 error")
+
+    movieContainer.appendChild(errorCard);
+    return;
+    
+  }
+// ------------------------------------------------
+
+
+
+
+
+//   ---------
+
+// --------------------------------------------------------
+
+
+ 
+
 }
+
+
+async function fetchData(){
+    const response = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=820658f7');
+    const datas = await response.json();
+    console.log(datas)
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+
+    var titlevar = document.createElement("h1");
+    titlevar.innerHTML = datas.Title;
+    card.appendChild(titlevar)
+
+    var typevar = document.createElement("h2");
+    typevar.innerHTML = datas.Type;
+    card.appendChild(typevar)
+
+    var yearvar = document.createElement("p")
+    yearvar.innerHTML = datas.Year;
+    card.appendChild(yearvar)
+
+    var image = document.createElement("img")
+    image.src = datas.Poster;
+    card.appendChild(image)
+
+    movieContainer.appendChild(card)
+}
+
+
 
 search.addEventListener("click",()=>{
     loadMoreData();
 })
 
+window.addEventListener('load',()=>{
+    fetchData()
+})
 
-//  fetchData()
+
